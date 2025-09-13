c3a9ee1f729221d55e0ae68563643d94f8f8afa1
zahra-ahmadbeigloo <zahraahmadbeigloo@gmail.com>
2025-09-13 11:03:50 +0100

    fix: typo and ensure Base in database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 # database.py
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 Base = declarative_base()
 
